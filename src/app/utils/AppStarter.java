89b34cfeb7bcea76238ca6e54037165831bd279a
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-11-23 14:30:27 +0200

    Fix mistyping.

--- src/app/utils/AppStarter.java
+++ src/app/utils/AppStarter.java
@@ -11,10 +11,10 @@ public class AppStarter {
         List<User> users = null;
         CommonAppController.getUsersInfo(users, 0, "Alice", "alice@test.com");
 
-        users = List.of(new User(1, "Tom", "tom@test.com"), new User(2, "John", "john@@test.com"));
+        users = List.of(new User(1, "Tom", "tom@test.com"), new User(2, "John", "john@test.com"));
         CommonAppController.getUsersInfo(users, 0, "Alice", "alice@test.com");
 
-        users = List.of(new User(0, "Alice", "alice@test.com"), new User(1, "Tom", "tom@test.com"), new User(2, "Tom", "another_tom@@test.com"));
-        CommonAppController.getUsersInfo(users, 1, "Tom", "another_tom@@test.com");
+        users = List.of(new User(0, "Alice", "alice@test.com"), new User(1, "Tom", "tom@test.com"), new User(2, "Tom", "another_tom@test.com"));
+        CommonAppController.getUsersInfo(users, 1, "Tom", "another_tom@test.com");
     }
 }
